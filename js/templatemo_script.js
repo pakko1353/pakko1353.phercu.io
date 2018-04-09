dd2eb6298aa3e1fe5727cc9978def57d8d69a012
Dalex <daniel254.cl@gmail.com>
2018-04-09 12:45:30 -0400

    Ajustando Mapa, por errores presentados. Eliminé dos redes sociales twitter y whatsapp

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -339,7 +339,7 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBTCb6ae_VYtnAIYe-p8SseSZqcm7JhPwI&v=3.exp&sensor=false&' + 'callback=initialize';
     document.body.appendChild(script);
 }
 
